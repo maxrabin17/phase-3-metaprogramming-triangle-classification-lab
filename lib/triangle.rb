efe7d0e813c9549a41b8088ce5157435a41ea932
maxrabin <max.rabin17@gmail.com>
2021-07-05 14:37:56 -0400

    commit tree 909de3e69a169f21b9081fb9b9456fbde26c4613

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    valid_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def valid_triangle
+    valid = [(a + b > c), (b + c > a), (c + a > b)]
+    [a, b, c].each do |side|
+      valid << false if side <= 0
+    raise TriangleError if valid.include?(false)
+    end
+    binding.pry
+  end
+
+  class TriangleError < StandardError
+  end
 end
